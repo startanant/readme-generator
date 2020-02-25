c68e0d91d054d86cb35e5c5af8f38525dc7b96fa
Anant S <startanant@gmail.com>
2020-02-25 18:48:03 -0500

    Adds inquirer prompts to get project details

--- index.js
+++ index.js
@@ -3,18 +3,79 @@ const inquirer = require("inquirer");
 
 async function main(){
     console.log( `README Generator` );
+
     const userRequest = await inquirer.prompt({
             type: "input",
             message: "What is your GitHub username?",
             name: "username"
-          })
+          });
 
     // get the user info
     const gitName = userRequest.username;
-    console.log(gitName);
+    //console.log(gitName);
     const apiGitUrl = `https://api.github.com/users/${gitName}`;
-    const userResponse = await axios.get( apiGitUrl );
-    console.log(userResponse.data);
+    try {
+        const userResponse = await axios.get( apiGitUrl );
+        console.log(userResponse.data);
+        const email = userResponse.data.email;
+        const image = userResponse.data.avatar_url;
+
+        //Get project details
+        const projectRequest = await inquirer.prompt([
+            {
+                type: "input",
+                message: "--Enter Project details-- \n Project Title: ",
+                name: "title"
+            },
+            {
+                type: "input",
+                message: "Description: ",
+                name: "description"
+            },
+            {
+                type: "input",
+                message: "License: ",
+                name: "license"
+            }
+        ]);
+
+        const projectTitle = projectRequest.title;
+        const projectDescription = projectRequest.description;
+        const license = projectRequest.license;
+        console.log(projectTitle + projectDescription + license);
+        
+
+
+    } catch (error) {
+        console.error(error.response.status + ' ' + error.response.statusText + '. Try again.');
+    }
+    
+    
+
+
+
+    //console.log(userResponse.data);
+    
+    
+    // if(userResponse.data.id) {
+    //     const email = userResponse.data.email;
+    //     const image = userResponse.data.avatar_url;
+    //     console.log("")
+    //     const readmeBuilder = await inquirer.prompt([
+    //     {
+    //         type: "input",
+    //         message: "What is your GitHub username?",
+    //         name: "username"
+
+    //     }
+    //     ]);
+    // } else {
+    //     console.log("User not found.")
+    // }
+    
+
+
+
 }
 
 main();
